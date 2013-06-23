454e39be12958e9c12f930986b86fbc5f7b306ee
Kazuhiro Sera <seratch@gmail.com>
2013-06-23 11:39:53 +0900

    Updated dependencies

--- project/Build.scala
+++ project/Build.scala
@@ -3,7 +3,8 @@ import play.Project._
 
 object DevTeamBuild extends Build {
 
-  lazy val scalikejdbcVersion = "1.6.3"
+  lazy val scalikejdbcVersion = "1.6.4"
+  lazy val h2Version = "1.3.172"
 
   lazy val common = Project(
     id = "common",
@@ -17,11 +18,11 @@ object DevTeamBuild extends Build {
         "com.github.seratch" %% "scalikejdbc"               % scalikejdbcVersion,
         "com.github.seratch" %% "scalikejdbc-config"        % scalikejdbcVersion,
         "com.github.seratch" %% "scalikejdbc-interpolation" % scalikejdbcVersion,
-        "org.slf4j"          %  "slf4j-simple"              % "[1.7,)"             % "provided",
+        "org.slf4j"          %  "slf4j-simple"              % "1.7.5"             % "provided",
         "org.hibernate"      %  "hibernate-core"            % "4.2.2.Final",
-        "com.h2database"     %  "h2"                        % "[1.3,)",
-        "com.github.seratch" %% "scalikejdbc-test"          % scalikejdbcVersion   % "test",
-        "org.specs2"         %% "specs2"                    % "[1.13,)"            % "test"
+        "com.h2database"     %  "h2"                        % h2Version,
+        "com.github.seratch" %% "scalikejdbc-test"          % scalikejdbcVersion  % "test",
+        "org.specs2"         %% "specs2"                    % "2.0"               % "test"
       )
     ) ++ net.virtualvoid.sbt.graph.Plugin.graphSettings
   )
@@ -32,13 +33,13 @@ object DevTeamBuild extends Build {
     val appDependencies = Seq(
       "com.github.seratch"   %% "scalikejdbc-play-plugin"         % scalikejdbcVersion,
       "com.github.seratch"   %% "scalikejdbc-play-fixture-plugin" % scalikejdbcVersion,
-      "org.json4s"           %% "json4s-ext"                      % "[3.2,)",
-      "com.github.tototoshi" %% "play-json4s-native"              % "[0.1,)",
-      "com.github.tototoshi" %% "play-flyway"                     % "[0.1,)",
-      "com.h2database"       %  "h2"                              % "[1.3,)"
+      "org.json4s"           %% "json4s-ext"                      % "3.2.4",
+      "com.github.tototoshi" %% "play-json4s-native"              % "0.1.0",
+      "com.github.tototoshi" %% "play-flyway"                     % "0.1.3",
+      "com.h2database"       %  "h2"                              % h2Version
     )
     play.Project(appName, appVersion, appDependencies, path = file("playapp")).settings(
-      (net.virtualvoid.sbt.graph.Plugin.graphSettings :+ (scalaVersion in ThisBuild := "2.10.1")):_*
+      (net.virtualvoid.sbt.graph.Plugin.graphSettings :+ (scalaVersion in ThisBuild := "2.10.2")):_*
     ).dependsOn(common)
   }
 
--- project/plugins.sbt
+++ project/plugins.sbt
@@ -12,4 +12,5 @@ addSbtPlugin("play" % "sbt-plugin" % "2.1.1")
 
 addSbtPlugin("net.virtual-void" % "sbt-dependency-graph" % "0.7.3")
 
+addSbtPlugin("com.timushev.sbt" % "sbt-updates" % "0.1.1")
 
